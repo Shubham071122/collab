4d570ad039f47749ba24336f73bc7c35cb70d352
Shubham071122 <shubhamkumar7112002@gmail.com>
2026-05-27 15:22:50 +0530

    feat: add canvas column to projects

---
+++ internal/database/migrations/20260527094043_update_projects_table.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE projects
+ADD COLUMN canvas JSONB DEFAULT '{"elements":[]}';
--- internal/project/dto.go
+++ internal/project/dto.go
@@ -10,3 +10,19 @@ type UpdateProjectRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
+
+// For the canvas, we can define a struct like this:
+// id
+// type
+// x
+// y
+// width
+// height
+// rotation
+// strokeColor
+// fillColor
+// strokeWidth
+// opacity
+// createdBy
+// createdAt
+// updatedAt
